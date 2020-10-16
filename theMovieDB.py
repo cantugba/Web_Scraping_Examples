7a399230bd36655209c5b66e53948feb46900449
tugbaca <tugbacan57@outlook.com>
2020-10-16 13:58:13 +0300

    get search results

--- theMovieDB.py
+++ theMovieDB.py
@@ -8,6 +8,10 @@ class theMoviveDb:
     def getPopulars(self):
         response = requests.get(f" {self.api_url }movie/popular?api_key={self.api_key}&language=en-US&page=1")
         return response.json()
+
+    def getSearchResults(self,keyword):
+        response = requests.get(f"{self.api_url}search/keyword?api_key={self.api_key}&query={keyword}&page=1")
+        return response.json()
     
     
 movieApi = theMoviveDb()
@@ -15,10 +19,15 @@ movieApi = theMoviveDb()
 while True:
     secim = input("1- Popular Movies \n 2-Search Movies\n 3-Exit \n Seçim: ")
     
-    if secim == "2":
+    if secim == "3":
         break
     else:
         if secim == "1":
             movies = movieApi.getPopulars()
             for movie in movies['results']:
               print(movie['title'])
+        elif secim == "2":
+            keyword = input("keyword: ")
+            movies = movieApi.getSearchResults(keyword)
+            for movie in movies['results']:
+              print(movie['name'])
